85058432e40109df5d5ec90b0464707337fc84f3
kcoombes-oakforduk-com <kcoombes@oakforduk.com>
2025-02-05 10:23:31 +0000

    add agent install

--- zabbix-proxy-v1.0.sh
+++ zabbix-proxy-v1.0.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
+#
 # curl -kLO https://raw.github.com/kcoombes-oakforduk-com/oakford-public/main/zabbix-proxy-v1.0.sh
+# chmod +x zabbix-proxy-v1.0.sh
+# ./zabbix-proxy-v1.0.sh
+#
 # Generate a key: powershell -command '$rndbytes = New-Object byte[] 16;(New-Object System.Security.Cryptography.RNGCryptoServiceProvider).GetBytes($rndbytes);($rndbytes |ForEach-Object ToString X2) -join {}'
+#
 
 read -p "Enter key: " key
 echo $key
@@ -45,7 +50,27 @@ fi
 echo "Creating key file"
 echo $key > /etc/zabbix/key
 
-echo "enable and start the zabbix-proxy service"
+echo "Enable and start the zabbix-proxy service"
 systemctl restart zabbix-proxy
 systemctl enable zabbix-proxy
+
+echo "Adding firewall exception"
+firewall-cmd --zone=public --add-port=10051/tcp --permanent
+firewall-cmd --reload
+
+echo "Install zabbix agent"
+yes | dnf install -y zabbix-agent
+
+echo "Re-writing config file"
+echo "PidFile=/run/zabbix/zabbix_agentd.pid" > /etc/zabbix/zabbix_agentd.conf
+echo "LogFile=/var/log/zabbix/zabbix_agentd.log" >> /etc/zabbix/zabbix_agentd.conf
+echo "LogFileSize=0" >> /etc/zabbix/zabbix_agentd.conf
+echo "Server=127.0.0.1" >> /etc/zabbix/zabbix_agentd.conf
+echo "ServerActive=127.0.0.1" >> /etc/zabbix/zabbix_agentd.conf
+echo "Hostname=$HOSTNAME" >> /etc/zabbix/zabbix_agentd.conf
+echo "Include=/etc/zabbix/zabbix_agentd.d/*.conf" >> /etc/zabbix/zabbix_agentd.conf
+
+echo "Enable and start the zabbix-agent service"
+systemctl restart zabbix-agent
+systemctl enable zabbix-agent
 
