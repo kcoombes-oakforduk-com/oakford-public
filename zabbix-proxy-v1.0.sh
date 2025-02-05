8898bf34e4f6a487f11cdb4cc0b837b1a84c2383
kcoombes-oakforduk-com <kcoombes@oakforduk.com>
2025-02-05 09:49:57 +0000

    add TLSConnect setting

--- zabbix-proxy-v1.0.sh
+++ zabbix-proxy-v1.0.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 # curl -kLO https://raw.github.com/kcoombes-oakforduk-com/oakford-public/main/zabbix-proxy-v1.0.sh
+# Generate a key: powershell -command '$rndbytes = New-Object byte[] 16;(New-Object System.Security.Cryptography.RNGCryptoServiceProvider).GetBytes($rndbytes);($rndbytes |ForEach-Object ToString X2) -join {}'
 
 read -p "Enter key: " key
 echo $key
@@ -19,6 +20,13 @@ setsebool -P httpd_can_connect_zabbix on
 
 echo "Updating conf file"
 sed -i -e "s/^\(Server=*\).*/\1185.73.67.34/" -e "s/^\(DBName=*\).*/\1\/tmp\/zabbix_proxy/" /etc/zabbix/zabbix_proxy.conf
+
+if grep "^TLSConnect" -i /etc/zabbix/zabbix_proxy.conf
+then 
+   sed -i "s/^\(TLSConnect=*\).*/\1psk/" /etc/zabbix/zabbix_proxy.conf;
+else
+   echo "TLSConnect=psk" >> /etc/zabbix/zabbix_proxy.conf;
+fi
 
 if grep "^TLSPSKIdentity" -i /etc/zabbix/zabbix_proxy.conf
 then 
